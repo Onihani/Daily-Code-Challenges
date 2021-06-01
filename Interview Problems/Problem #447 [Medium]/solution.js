38d37a9879102fa9ebc76b7f94d20c9178f03072
Onihani <50061326+Onihani@users.noreply.github.com>
2021-06-01 13:31:54 +0000

    Added Problem #447

---
+++ Interview Problems/Problem #447 [Medium]/solution.js
@@ -0,0 +1,14 @@
+/* 
+Implement integer exponentiation. That is, implement the pow(x, y) function, where x and y are integers and returns x^y.
+
+Do this faster than the naive method of repeated multiplication.
+
+For example, pow(2, 10) should return 1024.
+*/
+
+function pow (x, y) {
+  return x ** y;
+}
+
+// Testing Solution
+console.log(pow(2, 10)) // logs 1024
